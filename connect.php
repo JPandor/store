2b956c02b9fd5cc6b3fa44687bd05ee026550ae9
JPandor <88104928+JPandor@users.noreply.github.com>
2022-06-28 20:36:27 +0200

    added composer and slim

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "store";
+
+$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+?>
